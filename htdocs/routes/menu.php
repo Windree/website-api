1bde5b0143b051002407861ed85af94396f84730
Windree <windree@dragonsoul.com>
2020-10-25 13:06:10 +0000

    Remove  slash from menu url Slash is going to be added by fronend

--- htdocs/routes/menu.php
+++ htdocs/routes/menu.php
@@ -3,14 +3,14 @@
 
 $router->get('/api/menu', function () use ($router) {
     return response()->json(array(
-        array('text' => 'videos', 'url' => '/videos'),
-        array('text' => 'installations', 'url' => '/installations'),
-        array('text' => 'sculptures', 'url' => '/sculptures'),
-        array('text' => 'paintings', 'url' => '/paintings'),
-        array('text' => 'collaborations', 'url' => '/collaborations'),
-        array('text' => 'about', 'url' => '/about'),
-        array('text' => 'artist', 'url' => '/artist'),
-        array('text' => 'cv', 'url' => '/cv'),
-        array('text' => 'contact', 'url' => '/contact')
+        array('text' => 'videos', 'url' => 'videos'),
+        array('text' => 'installations', 'url' => 'installations'),
+        array('text' => 'sculptures', 'url' => 'sculptures'),
+        array('text' => 'paintings', 'url' => 'paintings'),
+        array('text' => 'collaborations', 'url' => 'collaborations'),
+        array('text' => 'about', 'url' => 'about'),
+        array('text' => 'artist', 'url' => 'artist'),
+        array('text' => 'cv', 'url' => 'cv'),
+        array('text' => 'contact', 'url' => 'contact')
     ));
 });
