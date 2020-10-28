680825c1dc59ecb246b477f7d8776152ab9b065d
Windree <windree@dragonsoul.com>
2020-10-28 10:15:29 +0000

    Return navigation from database

--- htdocs/routes/navigation.php
+++ htdocs/routes/navigation.php
@@ -2,15 +2,17 @@
 
 
 $router->get('/api/navigation', function () use ($router) {
-    return response()->json(array('items'=> array(0=>)
-        array('text' => 'videos', 'url' => 'videos'),
-        array('text' => 'installations', 'url' => 'installations'),
-        array('text' => 'sculptures', 'url' => 'sculptures'),
-        array('text' => 'paintings', 'url' => 'paintings'),
-        array('text' => 'collaborations', 'url' => 'collaborations'),
-        array('text' => 'about', 'url' => 'about'),
-        array('text' => 'artist', 'url' => 'artist'),
-        array('text' => 'cv', 'url' => 'cv'),
-        array('text' => 'contact', 'url' => 'contact')
-    ));
+    $results = app('db')->select("SELECT * FROM navigation n inner join page p on p.id=n.page_id");
+    return response()->json(array('items' => array_map(function ($value) {
+        return array(
+            'hide' => $value->hide,
+            'page' => array(
+                'name' => $value->name,
+                'title' => $value->title,
+                'description' => $value->description,
+                'keywords' => $value->keywords,
+                'date' => $value->date
+            )
+        );
+    }, $results)));
 });
